a376ab1af96146e292362f484f72cac44a72b2e2
Aaro55 <aaro.puranen@gmail.com>
2025-10-07 10:38:14 +0300

    Update Arvauspeli.java

--- Arvauspeli.java
+++ Arvauspeli.java
@@ -7,5 +7,25 @@ public class Arvauspeli {
     String salaisuus = "Emma";
     String muuttuja;
     int yritys = 0;
+
+     while (true) {
+            System.out.println("Guess my name (type stop to exit)");
+            muuttuja = scanner.nextLine();
+            
+
+            if (muuttuja.equalsIgnoreCase("stop")) {
+                
+                break;
+            }
+                yritys++;
+                
+            if (muuttuja.equalsIgnoreCase(salaisuus)) {
+                System.out.println("Congratulations!");
+                break;
+            }  
+        }
+
+        System.out.println("You guessed " + yritys + " times.");
+        scanner.close();
     }
 }
