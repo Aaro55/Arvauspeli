00af137c95053f3e69895a0ce3c32a4114f441e0
Aaro55 <aaro.puranen@gmail.com>
2025-10-07 10:36:54 +0300

    Update Arvauspeli.java

--- Arvauspeli.java
+++ Arvauspeli.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
+
 public class Arvauspeli {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    Scanner scanner = new Scanner(System.in);
     }
 }
