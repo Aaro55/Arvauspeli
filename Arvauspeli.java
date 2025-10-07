7cc72c2e5adb6bde68065657c9e98aae65711e36
Aaro55 <aaro.puranen@gmail.com>
2025-10-07 10:37:50 +0300

    Update Arvauspeli.java

--- Arvauspeli.java
+++ Arvauspeli.java
@@ -3,5 +3,9 @@ import java.util.Scanner;
 public class Arvauspeli {
     public static void main(String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
+
+    String salaisuus = "Emma";
+    String muuttuja;
+    int yritys = 0;
     }
 }
